939f3c3094e042734a99e4e4bde70c7d1441798a
waqar <waqarahmad90@gmail.com>
2014-10-23 21:57:31 +0500

    Project Created

---
+++ auto-management/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="auto_management.MvcApplication" Language="C#" %>
---
+++ auto-management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(auto_management.Startup))]
+namespace auto_management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
