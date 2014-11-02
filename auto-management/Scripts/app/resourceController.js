31b64caf4660d6bd4347fc722f7b4fdae383655e
waqar <waqarahmad90@gmail.com>
2014-11-03 01:34:46 +0500

    kjnkjdwq

--- auto-management/Scripts/app/resourceController.js
+++ auto-management/Scripts/app/resourceController.js
@@ -6,27 +6,7 @@
             var getResourse = {};
             var entityName = "StudentReg";
             
-            $http.get("/api/Resource/GetEntityData/" + entityName).success(function (data, status, headers, config) {
-                var headers = [];
-                for (var i = 0; i < data.headers.length; i++) {
-                    var obj = { "title": data.headers[i] };
-                    headers.push(obj);
-                }
-                var obj = {
-                        "title": "<a style='text-align:center' href=\"#\" data-toggle=\"modal\" data-target=\"#abc\" ng-click=\"openEntity('empty')\">Add</a>",
-                        sorting: false,
-                        "class" : "center"
-                }
-                headers.push(obj);
-               var $table = $('#example').dataTable({
-                   "data": data.lists,
-                   "paging": false,
-                   "columns": headers
-                      
-                });
-               $compile($table)($scope);
-               
-            });
+            drawTable(entityName,$http,$compile,$scope);
             $scope.openEntity = function (objectID) {
                 $http.get("/api/Resource/Get/" + entityName + "/" + objectID).success(function (data, status, headers, config) {
                     $.each(data.controlModelList, function (i, entity) {
@@ -46,14 +26,40 @@
             $scope.saveEntity = function () {
                 $http.post("/api/Resource", $scope.apiModels).success(function (data, status, headers, config) {
                     $("#abc").modal("hide");
-                   
+                    drawTable(entityName, $http, $compile, $scope);
                 }).error(function (data, status, headers, config) {
                     $scope.Error = "Oops... something went wrong";
                     $scope.working = false;
                 });
 
-
-
             }
         }]);
+
+function drawTable(entityName,$http,$compile,$scope)
+{
+    $http.get("/api/Resource/GetEntityData/" + entityName).success(function (data, status, headers, config) {
+        var headers = [];
+        for (var i = 0; i < data.headers.length; i++) {
+            var obj = { "title": data.headers[i] };
+            headers.push(obj);
+        }
+        var obj = {
+            "title": "<a style='text-align:center' href=\"#\" data-toggle=\"modal\" data-target=\"#abc\" ng-click=\"openEntity('empty')\">Add</a>",
+            sorting: false,
+            "class": "center"
+        }
+        headers.push(obj);
+        if ($.fn.dataTable.isDataTable('#example')) {
+            table = $('#example').DataTable().destroy();
+        }
+        var $table = $('#example').dataTable({
+            "data": data.lists,
+            "paging": false,
+            "columns": headers
+
+        });
+        $compile($table)($scope);
+
+    });
+}
 
